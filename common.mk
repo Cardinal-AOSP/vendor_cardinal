da0a8d7ec7b79dd6b6f3e555dba0c9074ac22bbb
Steve Kondik <steve@cyngn.com>
2016-08-07 23:21:29 -0700

    Tipsy: Add custom off-mode charging screen
      * Moving this to open-source from CyanogenOS :)
      * Thanks to Chao Chen, Ethan Chen, Pat Erley, Scott Mertz,
        and Keith Mok for various contributions to this.
    
    Change-Id: If79f9c279668d14ee0cde62889bb09e7185ef08c
    Signed-off-by: Adarsh-MR <adarshmr1998@gmail.com>
    
    Conflicts:
    	config/common.mk

--- common.mk
+++ common.mk
@@ -103,6 +103,15 @@ PRODUCT_PACKAGES += \
     strace \
     masquerade
 
+# Custom off-mode charger
+ifneq ($(WITH_CM_CHARGER),false)
+PRODUCT_PACKAGES += \
+    charger_res_images \
+    cm_charger_res_images \
+    font_log.png \
+    libhealthd.cm
+endif
+
 # Stagefright FFMPEG plugin
 PRODUCT_PACKAGES += \
     libffmpeg_extractor \
