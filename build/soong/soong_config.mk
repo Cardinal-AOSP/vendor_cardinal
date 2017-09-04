470633def806e484199fdaa8eb3ec6ec5030cb8d
Simon Shields <simon@lineageos.org>
2017-09-04 21:11:16 +1000

    build: soong: put cardinal variables in the "Cardinal" struct
    Change-Id: I9ed55cea0c55f0078f8f97ad4037b16ed11c0156

--- build/soong/soong_config.mk
+++ build/soong/soong_config.mk
@@ -1,5 +1,8 @@
+# Insert new variables inside the Cardinal structure
 cardinal_soong:
 	$(hide) mkdir -p $(dir $@)
 	$(hide) (\
 	echo '{'; \
+	echo '"Cardinal": {'; \
+	echo '},'; \
 	echo '') > $(SOONG_VARIABLES_TMP)
